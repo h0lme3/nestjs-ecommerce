e2d4f2a6f8054cd2a2f3e01b8c25c594cca4ebe6
haodev007 <haodev007@gmail.com>
2023-03-27 17:16:30 -0400

    basic user management feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/store'), ProductModule],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/store'), ProductModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import bcrypt from 'bcrypt';
+import { Model } from 'mongoose';
+import { UserSchema, UserDocument } from './schema/user.schema';
+import { CreateUserDTO } from './dtos/create-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(@InjectModel('User') private readonly userModel: Model<UserDocument>) {}
+
+  async addUser(createUserDTO: CreateUserDTO) {
+    const newUser = await this.userModel.create(createUserDTO);
+    newUser.password = await bcrypt.hash(newUser.password, 10);
+    return newUser.save();
+  }
+
+  async findUser(username: string) {
+    const user = await this.userModel.findOne({ username: username });
+    return user;
+  }
+}
